ef2cc0713b6d5079ef110d9f5995e1e541e6dc54
Musthaq Ahamad <musthu.gm@gmail.com>
2018-08-01 12:03:30 +0530

    initialized blog

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,9 +3,27 @@ import Link from 'gatsby-link'
 
 const EventsPage = () => (
   <div className="page">
-    <h1>Hi from the events page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <div className="container">
+      <section className="event-section">
+        <div className="event-list-item">
+          <p className="event-title">
+            Python BootCamp
+          </p>
+          <p className="event-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+
+        <div className="event-list-item">
+          <p className="event-title">
+            Python BootCamp
+          </p>
+          <p className="event-description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+      </section>
+    </div>
   </div>
 )
 
