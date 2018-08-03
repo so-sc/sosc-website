213cf201429c0d297eaec845c8d4663c57819077
Musthaq Ahamad <musthu.gm@gmail.com>
2018-08-03 11:25:57 +0530

    Added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,5 +22,19 @@ plugins: [
         maxWidth: 1080,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-101997744-5",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ],
 }
