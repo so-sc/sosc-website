e48a75a06c736218a720cf888ef1d680ca73687e
Musthaq Ahamad <musthu.gm@gmail.com>
2018-08-26 23:24:41 +0530

    Added team mebers

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import MemberCard from '../components/member_card';
+import PrakhyathImage from '../images/prakhyath_rai.jpg'
 
 function getMembers(data) {
   let members = [];
@@ -22,6 +23,21 @@ function getMembers(data) {
 const TeamsPage = ({data}) => (
   <div className="page">
     <div className="container">
+    <h2>Co-ordinators</h2>
+      <div className="team-section">
+        <div className="member-card">
+          <img className="profile-pic" src={PrakhyathImage}/>
+          <div className="member-details">
+              <h3 className="name">Prakhyath Rai</h3>
+              <p className="designation">Faculty Coordinator</p>
+          </div>
+        </div>
+          <MemberCard 
+            username="manjesh1"
+            full_name="Manjesh P Shetty"
+            designation="Mozilla Regional Coordinator"/>
+      </div>
+      <h2>Core Members</h2>
       <div className="team-section">
         { getMembers(data) }
       </div>
