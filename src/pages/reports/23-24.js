d28be5293fca365eede57a8e028f2307bde30cda
Deveesh Shetty <deveeshshetty@gmail.com>
2024-11-17 15:51:58 +0530

    fix: build errors

--- src/pages/reports/23-24.js
+++ src/pages/reports/23-24.js
@@ -2,6 +2,16 @@ import React from 'react'
 import report23_24 from '../../reports/year-23-24.pdf'
 
 export default function Report23_24() {
+  const [isClient, setIsClient] = React.useState(false)
+
+  React.useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  if (!isClient) {
+    return null
+  }
+
   return (
     <iframe
       src={report23_24}
