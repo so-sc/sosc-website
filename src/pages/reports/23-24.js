b2b54870716fc34029f9348a458927424976447b
Deveesh Shetty <deveeshshetty@gmail.com>
2024-11-17 16:16:19 +0530

    fix: pdf rendering errors in prod

--- src/pages/reports/23-24.js
+++ src/pages/reports/23-24.js
@@ -1,23 +1,15 @@
 import React from 'react'
-import report23_24 from '../../reports/year-23-24.pdf'
+import { withPrefix } from 'gatsby-link'
 
 export default function Report23_24() {
-  const [isClient, setIsClient] = React.useState(false)
-
-  React.useEffect(() => {
-    setIsClient(true)
-  }, [])
-
-  if (!isClient) {
-    return null
-  }
-
   return (
-    <iframe
-      src={report23_24}
-      title="SOSC Annual Report AY 23-24"
-      frameborder="0"
-      style={{ width: '100%', height: '100vh' }}
-    ></iframe>
+    <>
+      <iframe
+        src={withPrefix('/reports/year-23-24.pdf')}
+        title="SOSC Annual Report AY 23-24"
+        frameborder="0"
+        style={{ width: '100%', height: '100vh' }}
+      ></iframe>
+    </>
   )
 }
